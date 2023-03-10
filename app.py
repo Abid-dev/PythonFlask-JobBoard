c113f7372c4e23a05bd5f3c443424631992a1e5c
Abid <abdurRahman.abid@outlook.com>
2023-03-10 19:15:30 +0000

    restructure folders and files to align them with a standard flask project

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import datetime
 
 PATH = 'db/jobs.sqlite'
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder="templates")
 
 def open_connection():
     connection = getattr(g, '_connection', None)
@@ -31,7 +31,6 @@ def close_connection(exception):
 
 @app.route('/')
 @app.route('/jobs')
-
 def jobs():
     jobs = execute_sql('SELECT job.id, job.title, job.description, job.salary, employer.id as employer_id, employer.name as employer_name FROM job JOIN employer ON employer.id = job.employer_id')
     return render_template('index.html', jobs=jobs)
